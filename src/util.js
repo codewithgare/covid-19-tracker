6da37a13ed3adeca7cfc84b30a3f56303df7d87f
Gare Ophorokpa <103697651+techwithgaree@users.noreply.github.com>
2023-06-01 04:45:13 +0100

    fill color

--- src/util.js
+++ src/util.js
@@ -21,49 +21,48 @@ export const sortData = (data) => {
   // return sortedData
 };
 
-export const casesTypeColors = {
-  cases: {
+export const casesTypeColors = (casesType) =>{
+  switch (casesType.properties .type  ){
+  case "cases": 
+  return {
     hex: "#CC1034",
     rgb: "rgb(204, 16, 52)",
     half_op: "rgba(204, 16, 52, 0.5)",
     multiplier: 80,
-  },
-  recovered: {
+  };
+  case "recovered": 
+  return {
     hex: "#7dd71d",
     rgb: "rgb(125, 215, 29)",
     half_op: "rgba(125, 215, 29, 0.5)",
     multiplier: 120,
-  },
-  deaths: {
+  };
+  case "deaths": 
+  return {
     hex: "#fb4443",
     rgb: "rgb(251, 68, 67)",
     half_op: "rgba(251, 68, 67, 0.5)",
     multiplier: 200,
-  },
+  }
+
+}
 };
+
+
 
 //DRAW circles on the map with interactive tooltip
 export const showDataOnMap = (data, casesType = "cases") =>
-  {
-  const ChangeStyles
-  = (casesType) 
-  =>{
-    const circle = useMap()
-    circle.setStyle({
-fillColor: `${casesTypeColors[casesType].he}`
-    })
-  }
-  }
+  
 data.map((country) => (
     <Circle
       center={[country.countryInfo.lat, country.countryInfo.long]}
-      color={casesTypeColors[casesType].hex}
-      fillColor={casesTypeColors[casesType].hex}
+      
+      pathOptions ={casesTypeColors(casesType)}
       fillOpacity={0.4}
       radius={Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier}
     >
-      <ChangeStyles casessType={casesType}/>
-      <Popup>r
+      
+      <Popup>
         <div className="info-container">
           <div
             className="info-flag"
